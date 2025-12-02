6e0f865b715c748e66f713c5a128f4e51b599b63
Romanenko Vladimir <bonyshd@gmail.com>
2025-12-02 18:57:29 +0300

    сделал фичу с fastapi

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from grisha import poo
-from vova import foo
+from vova import foo, loo
  
 poo()
 foo()
 print(111111)
 print('œ∑´®†¥¨ˆø¬˚µ˜∫©ƒ∂ßå≈ç√∫˜µ≤µ˜∫©ƒ®†¥¨∆˚¬øˆ¨¥†®∂´∑®†©˙∆')
 print(1)
+loo()
--- vova.py
+++ vova.py
@@ -1,2 +1,7 @@
 def foo():
     print('Hello world')
+    for i in range(5):
+        print(i)
+        
+def loo():
+    import fastapi
